ad71292c791db5c414888bd3ae37a4c1cfefa935
luke-mendi02 <lukecmendiola@lewisu.edu>
2021-10-10 18:03:38 -0500

    Add all files

---
+++ printToPage.js
@@ -0,0 +1,16 @@
+var dice = {
+    roll: function () {
+      var randomNum = Math.floor(Math.random() * 7);
+      return randomNum;
+    }
+  }
+  function printNumber(number) {
+    var numSpot = document.getElementById('numSpot');
+    numSpot.innerHTML = number;
+  }
+  
+  var button = document.getElementById('button');
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+  };
